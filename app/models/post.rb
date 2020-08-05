d3946ab488ee719b7a56654e196e9d09eab6847c
Ignatius22 <igee54077@gmail.com>
2020-08-05 22:18:05 +0100

    add linter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 1000 }
 end
--- db/migrate/20200804123620_create_posts.rb
+++ db/migrate/20200804123620_create_posts.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :post
+      t.text :body
       t.timestamps
     end
   end
